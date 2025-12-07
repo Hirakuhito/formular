bb0af922902f9a7be4784fd7b03fe42553064df2
Hirakuhito <takuto.yoshino1210@icloud.com>
2025-12-07 10:35:07 +0900

    edit print

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 import pybullet as p
 import pybullet_data as pd
 
@@ -9,18 +11,23 @@ def main():
     p.setAdditionalSearchPath(pd.getDataPath())
     p.setGravity(0, 0, -9.8)
 
+    startPos=[0, 0, 1]
     startOrient=p.getQuaternionFromEuler([0, 0, 0])
 
     field = p.loadURDF("plane.urdf")
-    car = p.loadURDF("./formular_car/car.urdf", [0, 0, 0.1], startOrient)
+    car = p.loadURDF("./formular_car/car.urdf", startPos, startOrient)
     # car = p.loadURDF("test_car.urdf", [0, 0, 1], startOrient)
 
-    print(p.getNumJoints(car))
+    print("<=========================================================>")
+    print(f"car_joints : {p.getNumJoints(car)}")
     for i in range(p.getNumJoints(car)):
         print(p.getJointInfo(car, i))
+    print("<=========================================================>")
+
 
     while True:
         p.stepSimulation()
+        time.sleep(1/120)
 
 if __name__ == "__main__":
     main()
